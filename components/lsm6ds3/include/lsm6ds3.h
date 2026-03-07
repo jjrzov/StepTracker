dcfe81641dd12a741a6359dcc468d42dfc5b7f5c
jjrzov <jjrzov@gmail.com>
2026-03-06 23:06:44 -0800

    Copied existing driver code

---
+++ components/lsm6ds3/include/lsm6ds3.h
@@ -0,0 +1,10 @@
+#ifndef LSM6DS3_H
+#define LSM6DS3_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "driver/i2c_master.h"
+#include "driver/spi_master.h"
+#include "lsm6ds3_reg.h"
+
+#endif
